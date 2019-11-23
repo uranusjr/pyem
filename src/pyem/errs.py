8af40892469a5ded86cae4927fee46dbba8736e5
Tzu-ping Chung <uranusjr@gmail.com>
2019-11-24 03:20:21 +0800

    Add docstring and typing to explain @errorlog(...)

--- src/pyem/errs.py
+++ src/pyem/errs.py
@@ -14,16 +14,29 @@ class Error(enum.IntEnum):
     virtualenv_unavailable = enum.auto()
 
 
-_Error = typing.TypeVar("_Error")
-_ErrorHandler = typing.Callable[[_Error], int]
-_ErrorLogged = typing.Callable[..., int]
-_ErrorLogger = typing.Callable[[_ErrorLogged], _ErrorLogged]
+_Exc = typing.TypeVar("_Exc", bound=Exception)
+_ExcHandler = typing.Callable[[_Exc], int]
+_ExcLogged = typing.Callable[..., int]
+_ExcLogger = typing.Callable[[_ExcLogged], _ExcLogged]
 
 
-def errorlog(etype: typing.Type[_Error], hndl: _ErrorHandler) -> _ErrorLogger:
-    def decorator(f):
+def errorlog(etype: typing.Type[_Exc], hndl: _ExcHandler[_Exc]) -> _ExcLogger:
+    """Decorator to wrap the function in a try-except block.
+
+    This is essentially equivalent to::
+
+        try:
+            return f(*args, **kwargs)
+        except etype as e:
+            return hndl(e)
+
+    The intent is to convert a bubbled-up exception into a human-readable log
+    message and a proper error code, similar to how a web server converts an
+    exception into a 500 response.
+    """
+    def decorator(f: _ExcLogged) -> _ExcLogged:
         @functools.wraps(f)
-        def wrapped(*args, **kwargs):
+        def wrapped(*args, **kwargs) -> int:
             try:
                 return f(*args, **kwargs)
             except etype as e:
